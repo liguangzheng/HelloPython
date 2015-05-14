347d4d1e8353de0221508ef7ac4765494aaaa6fd
liguangzheng <liguangzhenghi@163.com>
2015-05-14 17:41:31 +0800

    HelloPython_first_commit

---
+++ src/classes/myTestClass.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2015年4月20日
+
+@author: liguangzheng
+'''
+class TestClass:
+    
+    def __init__(self, sayword):
+        self.sayword = sayword
+    
+    def sayHello(self):
+        print self.sayword
---
+++ src/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2015年4月20日
+
+@author: liguangzheng
+'''
+from classes import myTestClass
+testclass = myTestClass.TestClass('Hello! How are you?')
+testclass.sayHello()
